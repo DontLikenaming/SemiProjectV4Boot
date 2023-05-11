236224161ebb1a543753fd09d162bb957a520b6f
DontLikenaming <edirna21@gmail.com>
2023-05-11 15:58:43 +0900

    mvc 프로젝트 - lombok 설정

--- src/main/java/dontlikenaming/springboot/semiprojectv5/model/Zipcode.java
+++ src/main/java/dontlikenaming/springboot/semiprojectv5/model/Zipcode.java
@@ -1,5 +1,8 @@
 package dontlikenaming.springboot.semiprojectv5.model;
 
+import lombok.Data;
+
+@Data
 public class Zipcode {
     private String ZIPCODE;
     private String SIDO;
@@ -7,54 +10,4 @@ public class Zipcode {
     private String DONG;
     private String RI;
     private String BUNJI;
-
-    public Zipcode() {}
-
-    public String getZIPCODE() {
-        return ZIPCODE;
-    }
-
-    public void setZIPCODE(String ZIPCODE) {
-        this.ZIPCODE = ZIPCODE;
-    }
-
-    public String getSIDO() {
-        return SIDO;
-    }
-
-    public void setSIDO(String SIDO) {
-        this.SIDO = SIDO;
-    }
-
-    public String getGUGUN() {
-        return GUGUN;
-    }
-
-    public void setGUGUN(String GUGUN) {
-        this.GUGUN = GUGUN;
-    }
-
-    public String getDONG() {
-        return DONG;
-    }
-
-    public void setDONG(String DONG) {
-        this.DONG = DONG;
-    }
-
-    public String getRI() {
-        return RI;
-    }
-
-    public void setRI(String RI) {
-        this.RI = RI;
-    }
-
-    public String getBUNJI() {
-        return BUNJI;
-    }
-
-    public void setBUNJI(String BUNJI) {
-        this.BUNJI = BUNJI;
-    }
 }
